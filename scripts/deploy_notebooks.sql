9a15579cb680ee7d1d3808280f88df2eb8a5c33d
ketki-mude <132642080+ketki-mude@users.noreply.github.com>
2025-03-07 01:01:03 -0500

    Update deploy_notebooks.sql

--- scripts/deploy_notebooks.sql
+++ scripts/deploy_notebooks.sql
@@ -3,9 +3,9 @@
 CREATE OR REPLACE NOTEBOOK IDENTIFIER('"NOAA_DB"."{{env}}_SCHEMA"."{{env}}_05_load_csv_snowflake"')
     FROM '@"NOAA_DB"."INTEGRATIONS"."DEMO_GIT_REPO"/branches/"{{branch}}"/notebooks/05_load_csv_snowflake/'
     QUERY_WAREHOUSE = 'NOAA_WH'
-    MAIN_FILE = '05_load_csv_to_snowflake.ipynb';
+    MAIN_FILE = '05_load_csv_snowflake.ipynb';
 
-ALTER NOTEBOOK "NOAA_DB"."{{env}}_SCHEMA"."{{env}}_05_load_csv_to_snowflake" ADD LIVE VERSION FROM LAST;
+ALTER NOTEBOOK "NOAA_DB"."{{env}}_SCHEMA"."{{env}}_05_load_csv_snowflake.ipynb" ADD LIVE VERSION FROM LAST;
 
 CREATE OR REPLACE NOTEBOOK IDENTIFIER('"NOAA_DB"."{{env}}_SCHEMA"."{{env}}_06_harmonize_data"')
     FROM '@"NOAA_DB"."INTEGRATIONS"."DEMO_GIT_REPO"/branches/"{{branch}}"/notebooks/06_harmonize_data/'
