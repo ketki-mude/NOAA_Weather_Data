8adad7e5c0088202cdc7ab58956dcd6a057dd0fc
ketki-mude <132642080+ketki-mude@users.noreply.github.com>
2025-03-07 00:51:58 -0500

    Update deploy_notebooks.sql

--- scripts/deploy_notebooks.sql
+++ scripts/deploy_notebooks.sql
@@ -1,23 +1,22 @@
 --!jinja
 
 CREATE OR REPLACE NOTEBOOK IDENTIFIER('"NOAA_DB"."{{env}}_SCHEMA"."{{env}}_05_load_csv_snowflake"')
-    FROM '@"NOAA_DB"."INTEGRATIONS"."DEMO_GIT_REPO"/branches/"{{branch}}"/notebooks/05_load_csv_snowflake/'
+    FROM '@"NOAA_DB"."INTEGRATIONS"."DEMO_GIT_REPO"/branches/"{{branch}}"/notebooks/05_load_csv_snowflake/05_load_csv_to_snowflake.ipynb'
     QUERY_WAREHOUSE = 'NOAA_WH'
     MAIN_FILE = '05_load_csv_to_snowflake.ipynb';
 
 ALTER NOTEBOOK "NOAA_DB"."{{env}}_SCHEMA"."{{env}}_05_load_csv_snowflake" ADD LIVE VERSION FROM LAST;
 
 CREATE OR REPLACE NOTEBOOK IDENTIFIER('"NOAA_DB"."{{env}}_SCHEMA"."{{env}}_06_harmonize_data"')
-    FROM '@"NOAA_DB"."INTEGRATIONS"."DEMO_GIT_REPO"/branches/"{{branch}}"/notebooks/06_harmonize_data/'
+    FROM '@"NOAA_DB"."INTEGRATIONS"."DEMO_GIT_REPO"/branches/"{{branch}}"/notebooks/06_harmonize_data/06_harmonize_data.ipynb'
     QUERY_WAREHOUSE = 'NOAA_WH'
     MAIN_FILE = '06_harmonize_data.ipynb';
 
-ALTER NOTEBOOK "NOAA_DB"."{{env}}_SCHEMA"."{{env}}_07_harmonize_data" ADD LIVE VERSION FROM LAST;
+ALTER NOTEBOOK "NOAA_DB"."{{env}}_SCHEMA"."{{env}}_06_harmonize_data" ADD LIVE VERSION FROM LAST;
 
 CREATE OR REPLACE NOTEBOOK IDENTIFIER('"NOAA_DB"."{{env}}_SCHEMA"."{{env}}_07_analytics"')
-    FROM '@"NOAA_DB"."INTEGRATIONS"."DEMO_GIT_REPO"/branches/"{{branch}}"/notebooks/07_analytics/'
+    FROM '@"NOAA_DB"."INTEGRATIONS"."DEMO_GIT_REPO"/branches/"{{branch}}"/notebooks/07_analytics/07_analytics.ipynb'
     QUERY_WAREHOUSE = 'NOAA_WH'
     MAIN_FILE = '07_analytics.ipynb';
 
 ALTER NOTEBOOK "NOAA_DB"."{{env}}_SCHEMA"."{{env}}_07_analytics" ADD LIVE VERSION FROM LAST;
-
